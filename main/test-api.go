6f78b5c5baf952458694640391c84e12eb8e7d9c
JackJiaJJ <39511725+JackJiaJJ@users.noreply.github.com>
2022-01-13 15:01:52 +0800

    Create test-api.go

---
+++ main/test-api.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+        "api"
+        "fmt"
+        "github.com/gin-gonic/gin"
+)
+
+func main() {
+        fmt.Println("We will invoke api")
+        dbtype := "mysql"
+        dbinfo := "root:Zhu88jie!@tcp(x.xx.xxx.xxx:3306)/employee"
+
+        db, err := api.DBOpen(dbtype, dbinfo)
+        if err != nil {
+                fmt.Println("Failed to open db, error is -", err)
+                return
+        }
+
+        router := gin.Default()
+
+        api.GetAPI(db, router)
+        api.PostAPI(db, router)
+        api.DeleteAPI(db, router)
+        api.PutAPI(db, router)
+
+        router.Run("9.xx.xxx.xxx:8080")
+
+}
